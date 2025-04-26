258a2e12f098b50d075133070f43812560387c77
Madhavi-Patil <88056234+Madhavi-Patil@users.noreply.github.com>
2025-04-26 18:25:53 +0000

    Deploying to gh-pages from @ Madhavi-Patil/madhavi-patil.github.io@302bb96c29a2085999723c7a032ba6eb9d4232db 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -501,7 +501,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%67%73%68%69%72%73%65%6B@%75%6B%74.%65%64%75", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -522,7 +522,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=UL6yZWYAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
