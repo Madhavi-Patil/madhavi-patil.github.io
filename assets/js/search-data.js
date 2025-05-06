fe320c0a36f6bdcd84ed61310511ce4cc1595c31
Madhavi-Patil <88056234+Madhavi-Patil@users.noreply.github.com>
2025-05-06 13:52:01 +0000

    Deploying to gh-pages from @ Madhavi-Patil/madhavi-patil.github.io@9b0a17c8db76818e0e2c3f3d7d3af9128d597922 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-news",
-          title: "News",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/news/";
-          },
-        },{id: "nav-projects",
+  },{id: "nav-projects",
           title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
