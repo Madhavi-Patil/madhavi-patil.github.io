cd12432e20af3f9098e26f12e0b738badf0df90c
Madhavi-Patil <88056234+Madhavi-Patil@users.noreply.github.com>
2025-05-16 20:38:53 +0000

    Deploying to gh-pages from @ Madhavi-Patil/madhavi-patil.github.io@04d0e2d92889d071b814423a863d1a6d740fe7dc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -473,9 +473,9 @@ ninja.data = [{
           description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-plant-immune-system-evolution",
+          title: 'Plant Immune System Evolution',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
